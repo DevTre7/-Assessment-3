79522329a4515bfe3cbc19e843d584bad530d341
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2022-11-20 22:40:47 -0600

    second commit

--- about.js
+++ about.js
@@ -5,10 +5,30 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+alert("the form has been submitted!")
+
 }
 
 
 let form = document.querySelector('#contact');
+form.addEventListener("submit",handleSubmit);
 
-form.addEventListener('submit', handleSubmit);
+//form.addEventListener('submit', handleSubmit);
+//form.addEventListener('submit', handleSubmit);
+
+
+//Don't know why my event listener is not working properly?
+// /addEventListener('mouseover', (event) => {
+// 	alert("Have submited your answer to the question below?")
+// });
+//   onmouseover = (event) => { };
+
+//   */
+ let cat = document.querySelector("#cat")
+ function compliments()  {
+alert("You look MIGHTY fine today!");
+ }
+ cat.addEventListener("mouseover", compliments);
+
+ let spidercat = document.querySelector("#spider-cat")
+ spidercat.addEventListener("mouseover", compliments);
